e9b19a144e4c9dc8f763391348bb4284c70ee063
Maximilian Mellhage <m.mellhage@gmail.com>
2016-11-21 01:59:05 +0100

    .

--- XMLParsing/createXML3.py
+++ XMLParsing/createXML3.py
@@ -32,28 +32,60 @@ class cluster():
 		self.cluster = SubElement(clusters, "cluster", path=self.cluster_path)
 
 
-testlist = [['test', 'Forest'], ['201_L1_M3_S5', '0']]#, ['route 1', 'route 2', 'route 3', 'route 4', 'route 5']]]
-
+testlist = [['test', 'Forest'], ['201_L1_M3_S5', '0', ['route 1', 'route 2', 'route 3', 'route 4', 'route 5']], ["L_Mobcamp_HER_02", "L_01", "0", ["test", "test2"], "false"], ["L_Mobcamp_HER_02", "L_03", "4", ["test", "test2"]]]
+testlist2 = [['asd', 'Swamp'], ['asd', '76', ['route 1', 'route 2', 'route 3', 'route 4', 'route 5']], ["dgddgj", "L_08", "0", ["test", "test2"], "false"], ["isduosifd", "L_03", "4", ["test", "test2"]]]
 
 def break_down_list(clusterlist):
+	number_of_templates = len(testlist)-2
+	#print (number_of_templates)
+
+
 	cluster_object = cluster(clusterlist[0][0], clusterlist[0][1])
 	cluster_object.append_cluster() 
-	try: 
+
+	if len(clusterlist[1]) == 1:
+		cluster_object.add_master(clusterlist[1][0])
+	elif len(clusterlist[1]) == 2:
+		cluster_object.add_master(clusterlist[1][0], clusterlist[1][1])
+	elif len(clusterlist[1]) == 3:
 		cluster_object.add_master(clusterlist[1][0], clusterlist[1][1], clusterlist[1][2])
-	except:
+	elif len(clusterlist[1]) == 4:
+		cluster_object.add_master(clusterlist[1][0], clusterlist[1][1], clusterlist[1][2], clusterlist[1][3])
+	else:
 		print ("\n########################################\nLayers are missing in master template yo\n########################################\n")
 		sys.exit(0)
+
+	#try: 
+	#	cluster_object.add_master(clusterlist[1][0], clusterlist[1][1], clusterlist[1][2])
+	#except:
+	#	print ("\n########################################\nLayers are missing in master template yo\n########################################\n")
+	#	sys.exit(0)
+		
+	for t in clusterlist[2:]:
+		if len(t) == 3:
+			cluster_object.add_template(t[0], t[1], t[2])
+		elif len(t) == 4:
+			cluster_object.add_template(t[0], t[1], t[2], t[3])
+		elif len(t) == 5:
+			cluster_object.add_template(t[0], t[1], t[2], t[3], t[4]) 
+		else:
+			print ("\n########################################\nIncorrect amount of arguments in the template list\n########################################\n")
+
+		
+
 
 	
 
 
-	testing = []
-	for n in clusterlist[1]:
-		testing.append(n)
-		print(testing)
+	#testing = []
+	#for n in clusterlist[1]:
+	#	testing.append(n)
+	#	print(testing)
 
-	for n in clusterlist[]
-	cluster_object.add_template()
+
+
+	#for n in clusterlist[]
+	#cluster_object.add_template()
 
 
 
@@ -70,12 +102,13 @@ def break_down_list(clusterlist):
 	#print (test)
 
 break_down_list(testlist)
+break_down_list(testlist2)
 
-test = cluster("0001_SW", "Swamp")
-test.append_cluster()
-#test.add_master("002_L2_M3_S5")
-test.add_master("002_L2_M3_S5", "0", ["test", "test2"])
-test.add_template("L_Mobcamp_HER_01", "L_01", "0", ["test", "test2"])
+#test = cluster("0001_SW", "Swamp")
+#test.append_cluster()
+##test.add_master("002_L2_M3_S5")
+#test.add_master("002_L2_M3_S5", "0", ["test", "test2"])
+#test.add_template("L_Mobcamp_HER_01", "L_01", "0", ["test", "test2"])
 #
 #test2 = cluster("0002_SW", "Swamp")
 #test2.append_cluster()
