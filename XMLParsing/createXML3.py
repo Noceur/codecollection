32461be9f872ffdc97530862d9490a863dd21daf
Noceur <m.mellhage@gmail.com>
2016-11-25 19:02:33 +0100

    .

--- XMLParsing/createXML3.py
+++ XMLParsing/createXML3.py
@@ -2,25 +2,27 @@ from xml.etree.ElementTree import Element, SubElement, Comment
 #from lxml import Element, SubElement, Comment
 from ElementTree_pretty import prettify
 import sys
+import csv
+import ast
 
-clusters = Element("clusters")
+errors = []
 
 class cluster():
 	def __init__(self, cluster_name, biome):
 		self.cluster = ""
 		self.cluster_name = cluster_name
-		self.cluster_path = ("Assets/Levels/_EXPORT/ThirdGenerationWorld/" + cluster_name + ".cluster")
+		self.cluster_path = ("Assets/Levels/_EXPORT/ThirdGenerationWorld/" + cluster_name + ".unity")
 		self.master = ""
 		self.biome = biome
 		self.layer = ""
 
 	def add_master(self, master, orientation='0', layers=[], pos="0 0 0"):
-		self.master = SubElement(self.cluster, "master", path=("Assets/Levels/LEVEL_TEMPLATES/WORLD/" + self.biome + "/Master/" + master), position=pos, orientation=orientation)
+		self.master = SubElement(self.cluster, "master", path=("Assets/Levels/LEVEL_TEMPLATES/WORLD/" + self.biome + "/Master/" + master + ".unity"), position=pos, orientation=orientation)
 		if not len(layers) == 0:
 			self.add_layers(self.master, layers)
 
 	def add_template(self, template, id, orientation='0', layers=[], rotatewithmaster="true"):
-		self.template = SubElement(self.cluster, "template", id=id, rotatewithmaster=rotatewithmaster, path=("Assets/Levels/LEVEL_TEMPLATES/WORLD/" + self.biome + "/Slave/" + template), orientation=orientation)
+		self.template = SubElement(self.cluster, "template", id=id, rotatewithmaster=rotatewithmaster, path=("Assets/Levels/LEVEL_TEMPLATES/WORLD/" + self.biome + "/Slave/" + template + ".unity"), orientation=orientation)
 		if not len(layers) == 0:
 			self.add_layers(self.template, layers)
 
@@ -31,14 +33,8 @@ class cluster():
 	def append_cluster(self):
 		self.cluster = SubElement(clusters, "cluster", path=self.cluster_path)
 
-
-testlist = [['test', 'Forest'], ['201_L1_M3_S5', '0', ['route 1', 'route 2', 'route 3', 'route 4', 'route 5']], ["L_Mobcamp_HER_02", "L_01", "0", ["test", "test2"], "false"], ["L_Mobcamp_HER_02", "L_03", "4", ["test", "test2"]]]
-testlist2 = [['asd', 'Swamp'], ['asd', '76', ['route 1', 'route 2', 'route 3', 'route 4', 'route 5']], ["dgddgj", "L_08", "0", ["test", "test2"], "false"], ["isduosifd", "L_03", "4", ["test", "test2"]]]
-
 def break_down_list(clusterlist):
-	number_of_templates = len(testlist)-2
-	#print (number_of_templates)
-
+	number_of_templates = len(clusterlist)-2
 
 	cluster_object = cluster(clusterlist[0][0], clusterlist[0][1])
 	cluster_object.append_cluster() 
@@ -62,6 +58,8 @@ def break_down_list(clusterlist):
 	#	sys.exit(0)
 		
 	for t in clusterlist[2:]:
+		#if len(t) == 0:
+		#	
 		if len(t) == 3:
 			cluster_object.add_template(t[0], t[1], t[2])
 		elif len(t) == 4:
@@ -101,23 +99,33 @@ def break_down_list(clusterlist):
 
 	#print (test)
 
-break_down_list(testlist)
-break_down_list(testlist2)
+clusters = Element("clusters")
 
-#test = cluster("0001_SW", "Swamp")
-#test.append_cluster()
-##test.add_master("002_L2_M3_S5")
-#test.add_master("002_L2_M3_S5", "0", ["test", "test2"])
-#test.add_template("L_Mobcamp_HER_01", "L_01", "0", ["test", "test2"])
-#
-#test2 = cluster("0002_SW", "Swamp")
-#test2.append_cluster()
-##test.add_master("002_L2_M3_S5")
-#test2.add_master("003_L1_M4_S5", 0, ["test3", "test4"])
-#test2.add_template("L_Mobcamp_HER_03", "L_02", 0, ["test6", "tsta"])
-#test2.add_template("L_Mobcamp_HER_02", "L_01", 0, ["2", "s"])
-#test2.add_template("L_Mobcamp_HER_01", "L_03", 0, ["v", "9"])
+#with open('filename.csv') as csvfile:
+#	csvreader = csv.reader(csvfile)
+#	for row in csvreader:
+#		break_down_list(ast.literal_eval((row[0])))
+
+		#break_down_list(row[0])
+		#to_list = list(row[0])
+		#to_list = to_list[:-1]
+
+		#break_down_list(to_list)
+		#to_list = row[0]
+		#print (to_list)
+
+
+testlist4 = [['test', 'Forest'], ['201_L1_M3_S5', '270', ['route 2']], ['FR_Corner_01', 'B_05', '90', ['sea_sea_01']], ['FR_Straight_01', 'B_06', '90', ['sea_01']], ['FR_Exit_01', 'B_07', '90', ['sea_01']], ['FR_Straight_01', 'B_08', '90', []], ['FR_Corner_01', 'B_09', '180', ['sea_river_01']], ['FR_Straight_01', 'B_10', '180', ['river_01']], ['FR_Straight_01', 'B_11', '180', ['river_01']], ['FR_Straight_01', 'B_12', '180', ['river_01']], ['FR_Corner_01', 'B_13', '270', ['river_river_01']], ['FR_Straight_01', 'B_14', '270', ['river_01']], ['FR_Straight_01', 'B_15', '270', ['river_01']], ['FR_Straight_01', 'B_16', '270', ['river_01']], ['FR_Corner_01', 'B_01', '0', ['river_sea_01']], ['FR_Straight_01', 'B_02', '0', ['sea_01']], ['FR_Straight_01', 'B_03', '0', ['sea_01']], ['FR_Straight_01', 'B_04', '0', ['sea_01']]]
+testlist5 = [['test2', 'Forest'], ['201_L1_M3_S5', '0', ['route 1', 'route 3', 'route 4', 'route 5']], ['B_FR_Corner_01', 'B_01', '0', ['sea_sea_01']], ['B_FR_Straight_01', 'B_02', '0', ['sea_01']], ['B_FR_Exit_01', 'B_03', '0', ['sea_01']], ['B_FR_Straight_01', 'B_04', '0', ['sea_01']], ['B_FR_Corner_01', 'B_05', '90', ['sea_river_01']], ['B_FR_Straight_01', 'B_06', '90', ['river_01']], ['B_FR_Straight_01', 'B_07', '90', ['river_01']], ['B_FR_Straight_01', 'B_08', '90', ['river_01']], ['B_FR_Corner_01', 'B_09', '180', ['river_river_01']], ['B_FR_Straight_01', 'B_10', '180', ['river_01']], ['B_FR_Exit_01', 'B_11', '180', ['river_01']], ['B_FR_Straight_01', 'B_12', '180', ['river_01']], ['B_FR_Corner_01', 'B_13', '270', ['river_sea_01']], ['B_FR_Straight_01', 'B_14', '270', ['sea_01']], ['B_FR_Exit_01', 'B_15', '270', ['sea_01']], ['B_FR_Straight_01', 'B_16', '270', ['sea_01']]]
+
+break_down_list(testlist5)
 
 
 print (prettify(clusters))
+xml_file = open("cluster_generation.xml", "w")
+xml_file.write(prettify(clusters))
+xml_file.close()
+#tree.write("filename.xml")
 
+for e in errors:
+	print (e)
