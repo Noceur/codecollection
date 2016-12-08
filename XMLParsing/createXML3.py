6625abf36751ad73a1b81719f8c1bdf99486fc99
Noceur <m.mellhage@gmail.com>
2016-12-08 20:46:32 +0100

    Fucking typos

--- XMLParsing/createXML3.py
+++ XMLParsing/createXML3.py
@@ -101,10 +101,10 @@ def break_down_list(clusterlist):
 
 clusters = Element("clusters")
 
-#with open('filename.csv') as csvfile:
-#	csvreader = csv.reader(csvfile)
-#	for row in csvreader:
-#		break_down_list(ast.literal_eval((row[0])))
+with open('data.csv') as csvfile:
+	csvreader = csv.reader(csvfile)
+	for row in csvreader:
+		break_down_list(ast.literal_eval((row[0])))
 
 		#break_down_list(row[0])
 		#to_list = list(row[0])
@@ -117,7 +117,7 @@ clusters = Element("clusters")
 
 testlist5 = [['cluster_ID6', 'Swamp'], ['001_L1_M3_S5', '90', ['route_01_on', 'route_03_on', 'route_04_on', 'route_07_on', 'route_08_on', 'route_02_off', 'route_05_off', 'route_06_off',]], ['B_SW_Corner_01', 'B_01', '270', ['swamp_swamp_01']], ['B_SW_Straight_01', 'B_02', '0', ['swamp_02']], ['B_SW_Exit_01', 'B_03', '0', ['guardtower_outland_01']], ['B_SW_Straight_01', 'B_04', '0', ['swamp_02']], ['B_SW_Corner_01', 'B_05', '0', ['swamp_swamp_01']], ['B_SW_Straight_01', 'B_06', '90', ['swamp_02']], ['B_SW_Straight_01', 'B_07', '90', ['swamp_02']], ['B_SW_Straight_01', 'B_08', '90', ['swamp_02']], ['B_SW_Corner_01', 'B_09', '90', ['swamp_swamp_01']], ['B_SW_Straight_01', 'B_10', '180', ['swamp_02']], ['B_SW_Exit_01', 'B_11', '180', ['swamp_01']], ['B_SW_Straight_01', 'B_12', '180', ['swamp_02']], ['B_SW_Corner_01', 'B_13', '180', ['swamp_swamp_01']], ['B_SW_Straight_01', 'B_14', '270', ['swamp_01']], ['B_SW_Exit_01', 'B_15', '270', ['swamp_01']], ['B_SW_Straight_01', 'B_16', '270', ['swamp_02']],['SW_StreetCrossing', 'C_01', '180', ['route_I_01'], 'true'], ['SW_StreetCrossing', 'C_02', '0', ['NONE'], 'true'], ['SW_StreetCrossing', 'C_03', '180', ['route_L_01'], 'true'], ['SW_StreetCrossing', 'C_04', '180', ['route_T_01'], 'true']]
 
-break_down_list(testlist5)
+#break_down_list(testlist5)
 
 
 print (prettify(clusters))
--- networkgraph/graphtest.py
+++ networkgraph/graphtest.py
@@ -13,15 +13,15 @@ import re
 
 
 
-edge_list = ["N C_01", "route_01", "E C_01", "route_02", "S C_01", "route_03", "W C_01", "route_04"]
-nodes = ['N', 'E', 'S', 'W', 'C_01']
-edges = [('N', 'C_01'), ('E', 'C_01'), ('S', 'C_01'), ('W', 'C_01')]
-route_list = ['route_01', 'route_02', 'route_03', 'route_04']
-list_of_crossings = ['crossing_01']
-crossing_01 = ['route', 'C_01', 'route_01', 'route_02', 'route_03', 'route_04']
-
-
-
+edge_list = ["N C_01", "route_01", "E C_02", "route_02", "S C_03", "route_03", "W C_04", "route_04", "C_01 C_02", "route_05", "C_02 C_03", "route_06", "C_03 C_04", "route_07", "C_04 C_01", "route_08"]
+nodes = ['N', 'E', 'S', 'W', 'C_01', 'C_02', 'C_03', 'C_04', 'C_02', 'C_03', 'C_04']
+edges = [('N', 'C_01'), ('E', 'C_02'), ('S', 'C_03'), ('W', 'C_04'), ('C_01', 'C_02'), ('C_02', 'C_03'), ('C_03', 'C_04'), ('C_04', 'C_01'), ('C_02', 'C_03'), ('C_03', 'C_04'), ('C_04', 'C_01')]
+route_list = ['route_01', 'route_02', 'route_03', 'route_04', 'route_05', 'route_06', 'route_07', 'route_08']
+list_of_crossings = ['crossing_01', 'crossing_02', 'crossing_03', 'crossing_04']
+crossing_01 = ['route', 'C_01', 'route_01', 'route_05', 'route_08', 'none']
+crossing_02 = ['route', 'C_02', 'route_05', 'route_02', 'route_06', 'none']
+crossing_03 = ['route', 'C_03', 'route_07', 'route_06', 'route_03', 'none']
+crossing_04 = ['route', 'C_04', 'route_08', 'none', 'route_07', 'route_04']
 
 
 
@@ -235,7 +235,7 @@ def paths(): #probably something is fucked in here
     pathES = rem_bracket(pathES)
     pathEW = rem_bracket(pathEW)
     pathSW = rem_bracket(pathSW)
-    pathNES = rem_bracket(pathNE)
+    pathNES = rem_bracket(pathNES)
     pathESW = rem_bracket(pathESW)
     pathSWN = rem_bracket(pathSWN)
     pathNEW = rem_bracket(pathNEW)
