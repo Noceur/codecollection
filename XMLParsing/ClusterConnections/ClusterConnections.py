902a50d1ac87a18b78e7016d129a96b9800a0b88
Noceur <m.mellhage@gmail.com>
2017-02-21 19:26:20 +0100

    .

--- XMLParsing/ClusterConnections/ClusterConnections.py
+++ XMLParsing/ClusterConnections/ClusterConnections.py
@@ -1,64 +1,126 @@
 import lxml.etree as etree
+import sys
 from ElementTree_pretty import prettify
-
-def change_elem_attrib(searchattribstring, searchattrib, changeattribstring, changeattrib):
-    for item in element:
-        attribute = item.attrib
-
-        if searchattribstring in attribute[searchattrib]:
-            oldData = attribute[changeattrib]
-            attribute[changeattrib] = changeattribstring
-            print (attribute[searchattrib])
-            print (attribute[searchattrib] + "\t\t\t\told attribute value (" + changeattrib + "): " + oldData + "\t\t\t\tnew attribute value: " + changeattribstring)
 
 def write_to_xml(filename, towrite):
     f = open(filename, 'wb')
-    #print (etree.tostring(towrite, pretty_print=True))
     f.write(etree.tostring(towrite, pretty_print=True))
     f.close()
 
-def pp(e):
-    print (prettify(e))
-    #print (etree.tostring(e, pretty_print=True)) 
+def do_jobs(log_before=True, log_after=True):
+	for job in jobs:
+		print (job['ID'])
+		SourceCluster = cluster(job['ID'])
+		TargetCluster = cluster(job['TargetID'])
+		SourceCluster.get_exits()
+		TargetCluster.get_exits()
 
+		if log_before:
+			print ("\nexits before job:")
+			SourceCluster.print_exits()
+			TargetCluster.print_exits()
 
-doc = 'world.xml'
-tree = etree.parse(doc)
-root = tree.getroot()
-element = root[0]
+		if jobs[0]['Exit'] == 'N':
+			if jobs[0]['TargetExit'] == 'N':
+				print ("You are linking N to N exit(?)")
+				SourceCluster.exitN.attrib['targetid'] = (TargetCluster.exitN.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitN.attrib['targetid'] = (SourceCluster.exitN.attrib['id'] + "@" + SourceCluster.clusterID)
 
+			elif jobs[0]['TargetExit'] == 'E':
+				SourceCluster.exitN.attrib['targetid'] = (TargetCluster.exitE.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitE.attrib['targetid'] = (SourceCluster.exitN.attrib['id'] + "@" + SourceCluster.clusterID)
 
+			elif jobs[0]['TargetExit'] == 'S':
+				SourceCluster.exitN.attrib['targetid'] = (TargetCluster.exitS.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitS.attrib['targetid'] = (SourceCluster.exitN.attrib['id'] + "@" + SourceCluster.clusterID)
 
+			else:
+				SourceCluster.exitN.attrib['targetid'] = (TargetCluster.exitW.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitW.attrib['targetid'] = (SourceCluster.exitN.attrib['id'] + "@" + SourceCluster.clusterID)
 
+		elif jobs[0]['Exit'] == 'E':
+			if jobs[0]['TargetExit'] == 'N':
+				SourceCluster.exitE.attrib['targetid'] = (TargetCluster.exitN.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitN.attrib['targetid'] = (SourceCluster.exitE.attrib['id'] + "@" + SourceCluster.clusterID)
 
-#change_elem_attrib("4247", "file", "00:00", "timeregion")
+			elif jobs[0]['TargetExit'] == 'E':
+				print ("You are linking E to E exit(?)")
+				SourceCluster.exitE.attrib['targetid'] = (TargetCluster.exitE.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitE.attrib['targetid'] = (SourceCluster.exitE.attrib['id'] + "@" + SourceCluster.clusterID)
 
-#print (listOfTimezones[0])
-#for item in listOfTimezones:
-#    change_elem_attrib(item[0], "file", item[1], "timeregion")
+			elif jobs[0]['TargetExit'] == 'S':
+				SourceCluster.exitE.attrib['targetid'] = (TargetCluster.exitS.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitS.attrib['targetid'] = (SourceCluster.exitE.attrib['id'] + "@" + SourceCluster.clusterID)
+
+			else:
+				SourceCluster.exitE.attrib['targetid'] = (TargetCluster.exitW.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitW.attrib['targetid'] = (SourceCluster.exitE.attrib['id'] + "@" + SourceCluster.clusterID)
+
+		elif jobs[0]['Exit'] == 'S':
+			if jobs[0]['TargetExit'] == 'N':
+				SourceCluster.exitS.attrib['targetid'] = (TargetCluster.exitN.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitN.attrib['targetid'] = (SourceCluster.exitS.attrib['id'] + "@" + SourceCluster.clusterID)
+
+			elif jobs[0]['TargetExit'] == 'E':
+				SourceCluster.exitS.attrib['targetid'] = (TargetCluster.exitE.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitE.attrib['targetid'] = (SourceCluster.exitS.attrib['id'] + "@" + SourceCluster.clusterID)
+			elif jobs[0]['TargetExit'] == 'S':
+				print ("You are linking S to S exit(?)")
+				SourceCluster.exitS.attrib['targetid'] = (TargetCluster.exitS.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitS.attrib['targetid'] = (SourceCluster.exitS.attrib['id'] + "@" + SourceCluster.clusterID)
+			else:
+				SourceCluster.exitS.attrib['targetid'] = (TargetCluster.exitW.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitW.attrib['targetid'] = (SourceCluster.exitS.attrib['id'] + "@" + SourceCluster.clusterID)
+
+		else:
+			if jobs[0]['TargetExit'] == 'N':
+				SourceCluster.exitW.attrib['targetid'] = (TargetCluster.exitN.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitN.attrib['targetid'] = (SourceCluster.exitW.attrib['id'] + "@" + SourceCluster.clusterID)
+			elif jobs[0]['TargetExit'] == 'E':
+				SourceCluster.exitW.attrib['targetid'] = (TargetCluster.exitE.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitE.attrib['targetid'] = (SourceCluster.exitW.attrib['id'] + "@" + SourceCluster.clusterID)
+			elif jobs[0]['TargetExit'] == 'S':
+				SourceCluster.exitW.attrib['targetid'] = (TargetCluster.exitS.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitS.attrib['targetid'] = (SourceCluster.exitW.attrib['id'] + "@" + SourceCluster.clusterID)
+			else:
+				print ("You are linking W to W exit(?)")
+				SourceCluster.exitW.attrib['targetid'] = (TargetCluster.exitW.attrib['id'] + "@" + TargetCluster.clusterID)
+				TargetCluster.exitW.attrib['targetid'] = (SourceCluster.exitW.attrib['id'] + "@" + SourceCluster.clusterID)
+
+		if log_after:
+			print ("\nexits after job:")
+			SourceCluster.print_exits()
+			TargetCluster.print_exits()
+
+def load_xml(xml):
+	doc = xml
+	tree = etree.parse(doc)
+	root = tree.getroot()
+	element = root[0]
+	return element, tree
 
 class cluster():
 	def __init__(self, clusterID):
 		self.clusterID = clusterID
-		self.exitN = ""
-		self.exitE = ""
-		self.exitS = ""
-		self.exitW = ""
+		self.exitN = ''
+		self.exitE = ''
+		self.exitS = ''
+		self.exitW = ''
+		#print (self.clusterID)
 
-
-	def get_exits(self):
+	def get_exits(self, quiet=False):
 		for item in element:
 			printList = []
-			exitSide = ""
+			exitSide = ''
 			attribute = item.attrib
 			for content in item:
-				if attribute["id"] == self.clusterID:
-					print (item.tag, item.attrib["id"])
-
-					if content.tag == "exits":
-						print ("\t",content.tag)
+				if attribute['id'] == self.clusterID:
+					if content.tag == 'exits':
+						if not quiet == True:
+							print (item.tag, item.attrib['id'])
+							print ('\t',content.tag)
 						for exit in content:
-							if exit.attrib['targettype'] == "Cluster":
+							if exit.attrib['targettype'] == 'Cluster':
 								x = float(exit.attrib['pos'].split()[0])
 								y = float(exit.attrib['pos'].split()[1])
 
@@ -66,111 +128,90 @@ class cluster():
 
 
 								if x < y and y < (x*(-1)):
-									exitSide = "W"
+									#if exit.attrib['targetid'] != "00000000-0000-0000-0000-000000000000":
+										#print ("in use")
+										#sys.exit(("West exit in cluster", self.clusterID, "already has GUID assigned."))
+									exitSide = 'W'
 									self.exitW = exit
 								elif x > y and x > (y*(-1)):
-									exitSide = "E"
+									exitSide = 'E'
 									self.exitE = exit
 								elif y > x and y > (x*(-1)):
-									exitSide = "N"
+									exitSide = 'N'
 									self.exitN = exit
 								else:
-									exitSide = "S"
+									exitSide = 'S'
 									self.exitS = exit
 
 
 
 
-
-
-
-
-
-								print ("\t\texit pos:\t", x, y, exitSide, "\t\t", exit.attrib['targetid']) 
-								#printList.append(content[0].attrib)
-
-					#print (test.attrib)
-	print ()
+								#print ('\t\texit pos:\t', x, y, exitSide, '\t\t', exit.attrib['targetid']) 
+								if not quiet == True:
+									print ('\t\texit side:', exitSide,'\texit pos:', x, y, '\t\t', exit.attrib['targetid']) 
+		print ("")
 
 	def print_exits(self):
-		print (self.exitE, self.exitN, self.exitS, self.exitW)
+		print (self.clusterID)
+		print ("\t\tEXIT N-\t\t    id:", self.exitN.attrib['id'], "\t targetid:", self.exitN.attrib['targetid'])
+		print ("\t\tEXIT E-\t\t    id:", self.exitE.attrib['id'], "\t targetid:", self.exitE.attrib['targetid'])
+		print ("\t\tEXIT S-\t\t    id:", self.exitS.attrib['id'], "\t targetid:", self.exitS.attrib['targetid'])
+		print ("\t\tEXIT W-\t\t    id:", self.exitW.attrib['id'], "\t targetid:", self.exitW.attrib['targetid'])
 
+jobs = [
+{
+	'ID':			'2204',
+	'Exit':			'S',
+	'TargetID':		'1203',
+	'TargetExit':	'N'
+},
+{
+	'ID':			'2204',
+	'Exit':			'E',
+	'TargetID':		'1203#1',
+	'TargetExit':	'N'
+},
+{
+	'ID':			'1203',
+	'Exit':			'E',
+	'TargetID':		'1203#1',
+	'TargetExit':	'W'
+},
+{
+	'ID':			'1203',
+	'Exit':			'W',
+	'TargetID':		'2211',
+	'TargetExit':	'E'
+},
+{
+	'ID':			'1203',
+	'Exit':			'S',
+	'TargetID':		'2215',
+	'TargetExit':	'N'
+},
+{
+	'ID':			'1203#1',
+	'Exit':			'S',
+	'TargetID':		'2215',
+	'TargetExit':	'E'
+},
+{
+	'ID':			'2215',
+	'Exit':			'W',
+	'TargetID':		'2214',
+	'TargetExit':	'E'
+},
+{
+	'ID':			'2211',
+	'Exit':			'S',
+	'TargetID':		'2214',
+	'TargetExit':	'N'
+}
+]
 
+element, tree = load_xml('world.xml')
 
-cluster2214 = cluster("2214")
-cluster2214.get_exits()
+do_jobs(False, False)
 
-'''
-for item in element:
-	exitN = ""
-	exitE = ""
-	exitS = ""
-	exitW = ""
-	printList = []
-	exitSide = ""
-	attribute = item.attrib
-	for content in item:
-		if attribute["id"] == "2214":
-			print (item.tag, item.attrib["id"])
+#write_to_xml('world.xml', tree)
 
-			if content.tag == "exits":
-				print ("\t",content.tag)
-				for exit in content:
-					if exit.attrib['targettype'] == "Cluster":
-						x = float(exit.attrib['pos'].split()[0])
-						y = float(exit.attrib['pos'].split()[1])
-
-						
-
-
-						if x < y and y < (x*(-1)):
-							exitSide = "W"
-							exitW = exit
-						elif x > y and x > (y*(-1)):
-							exitSide = "E"
-							exitE = exit
-						elif y > x and y > (x*(-1)):
-							exitSide = "N"
-							exitN = exit
-						else:
-							exitSide = "S"
-							exitS = exit
-
-
-
-
-
-
-
-
-
-						print ("\t\texit pos:\t", x, y, exitSide, "\t\t", exit.attrib['targetid']) 
-						#printList.append(content[0].attrib)
-
-			#print (test.attrib)
-	print ()
-		#print (printList)
-#print (element)
-#write_to_xml("world.xml", tree)
-'''
-
-
-
-
-
-
-
-
-
-
-write_to_xml("world2.xml", tree)
-
-
-'''jobs = [
-	{
-		'ID': '0003',
-		'Exit': 'W',
-		'TargetID': '0004',
-		'TargetExit': 'E'
-	},
-]'''
