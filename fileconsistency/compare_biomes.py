8ac07dc624a6e72ba85d4a64551b53f1bc6b5c6e
Noceur <m.mellhage@gmail.com>
2017-03-09 18:41:52 +0100

    .

--- fileconsistency/compare_biomes.py
+++ fileconsistency/compare_biomes.py
@@ -7,6 +7,7 @@ class tile():
 
 	def __init__(self, name):
 		self.filename =		name
+		self.fileLoc =		""
 		self.fr =			False
 		self.mn =			False
 		self.hl =			False
@@ -31,12 +32,6 @@ def get_files():
 			#file = file[:-34]
 				print (file)
 
-def check_exception():
-	pass
-
-def try_print():
-	
-
 def loop_files():
 	counter = 0
 	exceptions = []
@@ -44,34 +39,69 @@ def loop_files():
 	for subdir, dirs, files in os.walk(currentDir):
 		#for file in files:
 		#	print (os.path.join(subdir, file))
-		if counter < 100:
+		#if counter < 1000:
 			for file in files:
-				if not ".meta" in file and not ".py" in file and not "_" in file[:1]:
-					counter += 1
-					stripped = re.findall("(?:.*?_){2}(.*)", file) #Matches everything after the second underscore
-					print ((re.search("(?<=_)(.*?)(?=_)", file)).group(0))
-					print ((re.search("[^_]*", file)).group(0))
-					tilejob = tile(stripped)
-					print (file)
-					#print (subdir)
-					#print (stripped)
+				fileBiome = ((re.search("[^_]*", file)))
+				fileLevel = ((re.search("(?<=_)(.*?)(?=_)", file)))
+				if  fileBiome.group(0).upper() == "FOREST" or fileBiome.group(0).upper() == "STEPPE" or fileBiome.group(0).upper() == "MOUNTAIN" or fileBiome.group(0).upper() == "HIGHLAND" or fileBiome.group(0).upper() == "SWAMP":
+					if  fileLevel.group(0).upper() == "GREEN" or fileLevel.group(0).upper() == "RED" or fileLevel.group(0).upper() == "DEAD":
+						if not ".meta" in file and not ".py" in file and not "_" in file[:1]:
+							counter += 1
+							stripped = re.findall("(?:.*?_){2}(.*)", file) #Matches everything after the second underscore
+							if fileBiome:
+								print (fileBiome.group(0))
+							if fileLevel:
+								print (fileLevel.group(0))
+							#print ((re.search("(?<=_)(.*?)(?=_)", file)).group(0))
+							tilejob = tile(stripped)
+							#print (file)
+							print (stripped[0])
+							#print (subdir)
+							#print (stripped)
 			#for directory in dirs:
 			#	print (directory)
 
 	print (counter)
 
+def search_list(item):
+	for prefab in prefab_list:
+		if item == prefab:
+			return True
+	else:
+		return False
 
-	def test2():
+def add_list(item, location, biome, style):
+	'''item_dict = {
+		prefab =		item,
+		fileLoc = 		location,
+		fr =			False,
+		mn =			False,
+		hl =			False,
+		st =			False,
+		sw = 			False,
+		frStyle =		"",
+		mnStyle =		"",
+		hlStyle =		"",
+		stStyle =		"",
+		swStyle =		""
+	}'''
 
-		for subdir, dirs, files in os.walk(currentDir):
-				for file in files:
-					if not ".meta" in file and not ".py" in file and not "_" in file[:1]:
+	item_dict = {'prefab': item}
 
-						stripped = re.findall("(?:.*?_){2}(.*)", file) 
-						print (file)
-						print (subdir)
+def add_biome_style():
+	pass
 
-		print (counter)
+def test2():
+	for subdir, dirs, files in os.walk(currentDir):
+			for file in files:
+				if not ".meta" in file and not ".py" in file and not "_" in file[:1]:
+
+					stripped = re.findall("(?:.*?_){2}(.*)", file) 
+					print (file)
+					print (subdir)
+
+	print (counter)
+	
 
 
 loop_files()
