5c2385a414659769bc5a43ae28069c45374d789c
Noceur <m.mellhage@gmail.com>
2017-04-27 18:03:01 +0200

    It now outputs two lists, one filtered and one unfiltered. It is half the speed because it runs everything twice which needs a fix and it also needs a indicator on whether the file can be found in any "_DO_NOT_MIGRATE" folders.

--- fileconsistency/compare_biomes.py
+++ fileconsistency/compare_biomes.py
@@ -3,44 +3,47 @@ import glob, os, re, csv, sys
 print (os.getcwd())
 currentDir = os.getcwd()
 
-def loop_files():
+def loop_files(filtered=False):
 	counter = 0
 	exceptions = []
 	#currentDir = ""
 	for subdir, dirs, files in os.walk(currentDir):
 		#for file in files:
 		#	print (os.path.join(subdir, file))
+		if "_DO_NOT_MIGRATE" in subdir.upper() and filtered == True:
+			print (subdir)
+			continue
 		#if counter < 8000:
-			for file in files:
-				fileBiome = ((re.search("[^_]*", file)))
-				fileLevel = ((re.search("(?<=_)(.*?)(?=_)", file)))
-				if  fileBiome.group(0).upper() == "FOREST" or fileBiome.group(0).upper() == "STEPPE" or fileBiome.group(0).upper() == "MOUNTAIN" or fileBiome.group(0).upper() == "HIGHLAND" or fileBiome.group(0).upper() == "SWAMP":
-					if  fileLevel.group(0).upper() == "GREEN" or fileLevel.group(0).upper() == "RED" or fileLevel.group(0).upper() == "DEAD":
-						if not ".meta" in file and not ".py" in file and not "_" in file[:1]:
-							#print (file[:-7])
-							dirCurrent = subdir.split(os.path.sep)[-1] #Get current directory
-							#print (subdir.split(os.path.sep)[-1])
-							counter += 1
-							stripped = re.findall("(?:.*?_){2}(.*)", file) #Matches everything after the second underscore
-							stripped = (stripped[0][:-7])
-							#stripped = stripped[0]
-							#if fileBiome:
-							#	print (fileBiome.group(0))
-							#if fileLevel:
-							#	print (fileLevel.group(0))
-							#print ((re.search("(?<=_)(.*?)(?=_)", file)).group(0))
-							#tilejob = tile(stripped)
-							#print (file)
-							#print (stripped[0])
-							#print (search_list(stripped))
-							search_list(stripped, subdir, fileBiome.group(0), fileLevel.group(0), dirCurrent)
-							#print (counter)
-							
+		for file in files:
+			fileBiome = ((re.search("[^_]*", file)))
+			fileLevel = ((re.search("(?<=_)(.*?)(?=_)", file)))
+			if  fileBiome.group(0).upper() == "FOREST" or fileBiome.group(0).upper() == "STEPPE" or fileBiome.group(0).upper() == "MOUNTAIN" or fileBiome.group(0).upper() == "HIGHLAND" or fileBiome.group(0).upper() == "SWAMP":
+				if  fileLevel.group(0).upper() == "GREEN" or fileLevel.group(0).upper() == "RED" or fileLevel.group(0).upper() == "DEAD":
+					if not ".meta" in file and not ".py" in file and not "_" in file[:1]:
+						#print (file[:-7])
+						dirCurrent = subdir.split(os.path.sep)[-1] #Get current directory
+						#print (subdir.split(os.path.sep)[-1])
+						counter += 1
+						stripped = re.findall("(?:.*?_){2}(.*)", file) #Matches everything after the second underscore
+						stripped = (stripped[0][:-7])
+						#stripped = stripped[0]
+						#if fileBiome:
+						#	print (fileBiome.group(0))
+						#if fileLevel:
+						#	print (fileLevel.group(0))
+						#print ((re.search("(?<=_)(.*?)(?=_)", file)).group(0))
+						#tilejob = tile(stripped)
+						#print (file)
+						#print (stripped[0])
+						#print (search_list(stripped))
+						search_list(stripped, subdir, fileBiome.group(0), fileLevel.group(0), dirCurrent)
+						#print (counter)
+						
 
-							#print (subdir)
-							#print (stripped)
-			#for directory in dirs:
-			#	print (directory)
+						#print (subdir)
+						#print (stripped)
+		#for directory in dirs:
+		#	print (directory)
 
 	print (counter)
 
@@ -76,6 +79,20 @@ def add_list(item, location, biome, style, dirCurrent):
 	item_dict[(biome_abr(biome)+'Style')].append(style)
 
 	prefabList.append(item_dict)
+
+def add_biome_style(item, location, biome, style, dirCurrent):	
+	style = style_abr(style)
+
+	# remove biome and level (red, dead, green) from dirCurrent
+	dirCurrent = strip_dirCurrent(dirCurrent)
+
+	for entry in prefabList:
+		if item == entry['prefab']:
+			if entry['dirCurrent'] != dirCurrent:
+				entry['locConsistency'] = False
+			entry['location'].append(location) 
+			entry[biome_abr(biome)] = True
+			entry[(biome_abr(biome)+'Style')].append(style)
 
 def style_abr(style):
 	if style.upper() == "GREEN":
@@ -125,20 +142,6 @@ def biome_abr(biome):
 		print ("Couldn't find biome abr.")
 		input('Press enter to continue: ')
 
-def add_biome_style(item, location, biome, style, dirCurrent):
-	style = style_abr(style)
-
-	# remove biome and level (red, dead, green) from dirCurrent
-	dirCurrent = strip_dirCurrent(dirCurrent)
-
-	for entry in prefabList:
-		if item == entry['prefab']:
-			if entry['dirCurrent'] != dirCurrent:
-				entry['locConsistency'] = False
-			entry['location'].append(location) 
-			entry[biome_abr(biome)] = True
-			entry[(biome_abr(biome)+'Style')].append(style)
-
 def data_formater(entry, biome, printStyle, appendLine, silent=True): #Makes sure the
 	biomeAbr = biome_abr(biome)
 
@@ -161,14 +164,24 @@ def data_formater(entry, biome, printStyle, appendLine, silent=True): #Makes sur
 
 	return appendLine
 
-def data_output(silent=True):
+def data_output(silent=True, onlyMissing=False):
+	writeList = []
 	printStyle = "";
 	appendLine = ""
+	addLine = True
 	for each in prefabList:
 		for entry in each['prefab']:
 			appendLine += entry
 			if not silent:
 				print (entry)
+
+
+
+			#print (each)
+			#if "DO_NOT_MIGRATE" in str(each['location'][0]).upper():
+			#	print (each['location'][0])
+
+
 		appendLine += ("\t"+each['dirCurrent'])
 
 		appendLine = data_formater(each, "SWAMP", printStyle, appendLine, silent)
@@ -176,6 +189,12 @@ def data_output(silent=True):
 		appendLine = data_formater(each, "MOUNTAIN", printStyle, appendLine, silent)
 		appendLine = data_formater(each, "HIGHLAND", printStyle, appendLine, silent)
 		appendLine = data_formater(each, "FOREST", printStyle, appendLine, silent)
+
+		#print (appendLine)
+		if onlyMissing:
+			if "SWAMP	 D G R" in appendLine and "STEPPE	 D G R" in appendLine and "MOUNTAIN	 D G R" in appendLine and "HIGHLAND	 D G R" in appendLine and "FOREST	 D G R" in appendLine:
+				print (appendLine)
+				addLine = False
 
 		if not each['locConsistency']:
 			appendLine += "\t"
@@ -185,12 +204,8 @@ def data_output(silent=True):
 				appendLocation += ('"' + entry + '"')
 				if not (i+1) >= len(each['location']):
 					appendLocation += (", CHAR(10), ")
-				#print (len(each['location']))
-				#print (i)
-				#appendLocation += entry
 			appendLocation += ")"
 			appendLocation = appendLocation.replace("\\", "/")
-			#print (appendLocation)
 			appendLine += appendLocation
 
 		if not silent:
@@ -198,24 +213,33 @@ def data_output(silent=True):
 				print (entry)
 			print ("\n")
 
-		writeList.append(appendLine)
+		if addLine:
+			writeList.append(appendLine)
+		addLine = True
 		appendLine = ""
 
-writeList = []
+	return writeList
+
+def output_file(filename, outputList):
+	f = open(filename, "wt", newline='')
+	try:
+		#writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar=',')
+		writer = csv.writer(f, quoting=csv.QUOTE_NONE, delimiter='|', quotechar='',escapechar='\\')
+		#writer = csv.writer(f)
+		for entry in outputList:
+			writer.writerow([entry])
+	finally: 
+		f.close()
+
+
+#prefabList = []
+#loop_files()
+#print ("\n\n\n\n")
+#unfilteredOutput = data_output(True)
+#output_file("output_unfiltered.csv", unfilteredOutput)
+
 prefabList = []
-loop_files()
+loop_files(True)
 print ("\n\n\n\n")
-#data_output()
-data_output(True)
-
-
-
-f = open("output.csv", "wt", newline='')
-try:
-	#writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar=',')
-	writer = csv.writer(f, quoting=csv.QUOTE_NONE, delimiter='|', quotechar='',escapechar='\\')
-	#writer = csv.writer(f)
-	for entry in writeList:
-		writer.writerow([entry])
-finally: 
-	f.close()
+filteredOutput = data_output(True, True)
+output_file("output_filtered.csv", filteredOutput)
