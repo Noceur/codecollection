e89973c069ba5e657fe9ba464393e8588c005e62
Noceur <m.mellhage@gmail.com>
2017-06-30 18:09:04 +0200

    .

--- heatmap_death/heatmap_gen.py
+++ heatmap_death/heatmap_gen.py
@@ -5,6 +5,22 @@ import matplotlib.pyplot as plt
 from scipy import misc
 import scipy
 from data import *
+from PIL import Image, ImageEnhance
+
+
+
+
+#fig, ax = plt.subplots()
+#ax.plot(range(10))
+#
+#for item in [fig, ax]:
+#	item.patch.set_visible(False)
+
+#fig.show()
+#with open('test2.png', 'w') as outfile:
+#	fig.canvas.print_png(outfile)
+
+#ax.show()
 
 # Generate some test data
 #x = np.random.randn(8873)
@@ -17,7 +33,26 @@ extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 #img = imread(map_fishybusiness.png)
 img = misc.imread("map_fishybusiness_live.bmp")
 
-plt.clf()
+#extent = []
+#extent.append(0)
+#extent.append(450)
+#extent.append(0)
+#extent.append(450)
+print (extent)
+
+#plt.clf() # clears current figure
 #plt.scatter(x,y,img)
 plt.imshow(heatmap.T, extent=extent, origin='lower')
+plt.savefig('test.png',bbox_inches='tight')
+
 plt.show()
+
+
+img2 = Image.open('test.png')
+img1 = Image.open('map_fishybusiness.png')
+img2.putalpha(ImageEnhance.Brightness(img2.split()[3]).enhance(0.5))
+img1 = Image.composite(img2, img1, img2)
+#img1.save('out.png')
+#img1.show()
+
+#plt.show()
